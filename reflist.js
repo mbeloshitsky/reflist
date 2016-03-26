d565b44ccff1005ff83881968c2b22434a621dd7
Michel Beloshitsky <mbeloshitsky@gmail.com>
2016-03-26 13:15:07 +0500

    Prune Cluster

--- reflist.js
+++ reflist.js
@@ -1,82 +1,112 @@
-var reflist = [
-		{
-			system: 		'ЭЦ-МПК' 
-			, title: 		'ст. Пикалево–1'
-			, arrows: 		25
-			, deployed_on: 	'2001-11-01'
-			, deployed_by: 	'ЦКЖТ'
-			, latlng: 		[59.557095, 34.083609]
-		},
-		{
-			system: 		'ЭЦ-МПК' 
-			, title: 		'ст. Муслюмово'
-			, arrows: 		21
-			, deployed_on: 	'2006-11-01'
-			, deployed_by: 	'НИЛ КСА'
-			, latlng: 		[55.58582, 61.597578]
-		},
-		{
-			system: 		'ЭЦ-МПК' 
-			, title: 		'ст. Юргамыш'
-			, arrows: 		19
-			, deployed_on: 	'2012-12-01'
-			, deployed_by: 	'НИЛ КСА'
-			, latlng: 		[55.369694, 64.457895]
-		},
-		{
-			system: 		'СТД-МПК' 
-			, title: 		'ст. Кыштым'
-			, arrows: 		42
-			, deployed_on: 	'2016-01-01'
-			, deployed_by: 	'НИЛ КСА'
-			, latlng: 		[55.726665, 60.55068]
-		},
-		{
-			system: 		'ЭЦ-МПК' 
-			, title: 		'ст. Кыштым'
-			, notes: 		'+ переезд АПС МП-Н в 02.2013'
-			, arrows: 		42
-			, deployed_on: 	'2005-12-01'
-			, deployed_by: 	'НИЛ КСА'
-			, latlng: 		[55.726675, 60.55078]
-		}
-	]
-	, systemInfo = [
-	 	{
-	 		name: 	'ЭЦ-МПК'
-			, icon:   'marker-ec-mpk.png'
-			, enabled: true
-		},
-		{
-			name: 'ЭЦ-МПК-У'
-			, icon: 'marker-ec-mpk-u.png'
-			, enabled: true
-		},
-		{	
-			name: 'СТД-МПК'
-			, icon: 'marker-std-mpk.png'
-			, enabled: true
-		}
-	]
-	, deployMap = null;
+var deployMap = null;
+var pruneCluster = new PruneClusterForLeaflet();
 
-function mkIcon (url) {
+pruneCluster.BuildLeafletClusterIcon = function(cluster) {
+    var e = new L.Icon.MarkerCluster();
+
+    e.stats = cluster.stats;
+    e.population = cluster.population;
+    return e;
+};
+
+L.Icon.MarkerCluster = L.Icon.extend({
+    options: {
+        iconSize: new L.Point(48, 48),
+        className: 'prunecluster leaflet-markercluster-icon'
+    },
+
+    createIcon: function () {
+        // based on L.Icon.Canvas from shramov/leaflet-plugins (BSD licence)
+        var e = document.createElement('canvas');
+        this._setIconStyles(e, 'icon');
+        var s = this.options.iconSize;
+        e.width = s.x;
+        e.height = s.y;
+        this.draw(e.getContext('2d'), s.x, s.y);
+        return e;
+    },
+
+    createShadow: function () {
+        return null;
+    },
+
+    draw: function(canvas, width, height) {
+        canvas.beginPath();
+        canvas.fillStyle = 'white';
+        canvas.arc(24, 24, 16, 0, Math.PI*2);
+        canvas.fill();
+        canvas.closePath();
+
+        var a = 0;
+        for (var i in systemInfo) {
+            var size = this.stats[i] / this.population
+              , sector_a = Math.PI*2/6;
+            if (size > 0) {
+            	a += 1
+            	canvas.beginPath();
+            	canvas.moveTo(24,24);
+				canvas.arc(24,24, 24, sector_a*a, sector_a*(a+1));
+				canvas.lineTo(24,24);
+                canvas.fillStyle = systemInfo[i].color;
+                canvas.fill();
+                canvas.closePath();
+
+                /* canvas.beginPath();
+
+                var angle = Math.PI/4*a;
+                var posx = Math.cos(angle) * 18, posy = Math.sin(angle) * 18;
+
+
+                var xa = 0, xb = 1, ya = 4, yb = 8;
+
+                // var r = ya + (size - xa) * ((yb - ya) / (xb - xa));
+                var r = ya + size * (yb - ya);
+
+                //canvas.moveTo(posx, posy);
+                canvas.arc(24+posx,24+posy, r, 0, Math.PI*2);
+                canvas.fillStyle = systemInfo[i].color;
+                canvas.fill();
+                canvas.closePath(); */
+            }
+        }
+
+        
+        canvas.fillStyle = '#555';
+        canvas.textAlign = 'center';
+        canvas.textBaseline = 'middle';
+        canvas.font = 'bold 12px sans-serif';
+
+        canvas.fillText(this.population, 24, 24, 48);
+    }
+});
+
+
+
+
+
+/* function iconName(systems) {
+	var compositeName = systems
+		.map(function (sys) { return systemInfo[sys].icon })
+		.join('-');
+	return 'marker-'+compositeName+'.png'
+}
+
+
+function mkIcon (systems) {
 	return L.icon({ 
-		iconUrl: 		url, 
+		iconUrl: 		iconName(systems), 
 		iconSize: 		[20, 32.5],
 		shadowUrl: 		'marker-shadow.png',
 		shadowSize: 	[30, 30],
     	shadowAnchor: 	[10, 15]
 	})
-}
+} */
 
 function showDeployDate (dd) {
 	var d = new Date(dd);
 	return d.getMonth() + '.'+ d.getFullYear(); 
 }
 
-
- 
 document.addEventListener("DOMContentLoaded", function(e){
 	var vue = new Vue({
 		el: '#app',
@@ -86,26 +116,31 @@ document.addEventListener("DOMContentLoaded", function(e){
 		},
 		computed: {
 			'selected': function (reflist) {
-				var self = this, enabled = {};
-				self.systems.forEach(function (sys) { enabled[sys.name] = sys.enabled; })
-				return self.reflist.filter(function (obj) { return enabled[obj.system] })
+				var self = this;
+				function hasEnabled (obj) {
+					return obj
+						.systems
+						.filter(function (sys) { 
+							return self.systems[sys].enabled; 
+						}).length > 0;
+				}
+				return self.reflist.filter(hasEnabled)
 			}
 		},
 		watch: {
 			selected: function updateMap(reflist) {
-				var self = this, icons = {}
-				systemInfo.forEach(function (sinfo) {
-					icons[sinfo.name] = sinfo.icon;
-				})
-				reflist.forEach(function (obj) {
-					L.marker(obj.latlng, {icon: mkIcon(icons[obj.system]) })
-						.addTo(deployMap)
-			    		.bindPopup(
-			    			'<b>'+obj.title+'</b>'
-			    			+' '+showDeployDate(obj.deployed_on)
-			    			+'<br />'+plural(obj.arrows,'стрелка', 'стрелки', 'стрелок')
-			    			+(obj.notes ? ('<br />'+ obj.notes) : ''));
-				});
+				pruneCluster.RemoveMarkers();
+				var self = this
+				return;
+				/* L.marker(obj.latlng, {icon: mkIcon(obj.systems) })
+				.addTo(deployMap)
+	    		.bindPopup(
+	    			'<b>'+obj.title+'</b>'
+	    			+' '+showDeployDate(obj.deployed_on)
+	    			+'<br />'+obj.systems.join(' + ')
+	    			+'<br />'+plural(obj.arrows,'стрелка', 'стрелки', 'стрелок')
+	    			+(obj.notes ? ('<br />'+ obj.notes) : '')); */
+
 			}
 		}
 	})
@@ -115,6 +150,19 @@ document.addEventListener("DOMContentLoaded", function(e){
 	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 	    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 	}).addTo(deployMap);
+
+	deployMap.addLayer(pruneCluster);
+
+	reflist.forEach(function (obj) {
+		obj.systems.forEach(function (sys) {
+			var marker = new PruneCluster.Marker(obj.latlng[0], obj.latlng[1]);
+			marker.category = sys;
+			pruneCluster.RegisterMarker(marker);						
+		})
+	});
+
+	pruneCluster.ProcessView();
+
 })
 
 function plural (a, str1, str2, str3) {
@@ -130,3 +178,11 @@ function plural (a, str1, str2, str3) {
 Vue.filter('plural', plural)
 
 Vue.filter('json', function (a) { return JSON.stringify(a) })
+
+Vue.filter('join', function (values) { return values.join(', ') })
+
+Vue.filter('icon', function (values) { 
+	if (values.map === undefined)
+		values = [values];
+	return iconName(values);
+})
---
+++ stations.js
@@ -0,0 +1,52 @@
+var reflist = [
+	{
+		systems: 		['ЭЦ-МПК'] 
+		, title: 		'ст. Пикалево–1'
+		, arrows: 		25
+		, deployed_on: 	'2001-11-01'
+		, deployed_by: 	'ЦКЖТ'
+		, latlng: 		[59.557095, 34.083609]
+	},
+	{
+		systems: 		['ЭЦ-МПК'] 
+		, title: 		'ст. Муслюмово'
+		, arrows: 		21
+		, deployed_on: 	'2006-11-01'
+		, deployed_by: 	'НИЛ КСА'
+		, latlng: 		[55.58582, 61.597578]
+	},
+	{
+		systems: 		['ЭЦ-МПК'] 
+		, title: 		'ст. Юргамыш'
+		, arrows: 		19
+		, deployed_on: 	'2012-12-01'
+		, deployed_by: 	'НИЛ КСА'
+		, latlng: 		[55.369694, 64.457895]
+	},
+	{
+		systems: 		['ЭЦ-МПК', 'СТД-МПК'] 
+		, title: 		'ст. Кыштым'
+		, notes: 		'+ переезд АПС МП-Н в 02.2013'
+		, arrows: 		42
+		, deployed_on: 	'2005-12-01'
+		, deployed_by: 	'НИЛ КСА'
+		, latlng: 		[55.726675, 60.55078]
+	}
+]
+, systemInfo = {
+ 	'ЭЦ-МПК': {
+ 	 	icon:   'ec-mpk'
+		, enabled: true
+		, color: '#ff4b00'
+	}
+	, 'ЭЦ-МПК-У': {
+		icon: 'ec-mpk-u'
+		, enabled: true
+		, color: '#bac900'
+	}
+	, 'СТД-МПК': {	
+		icon: 'std-mpk'
+		, enabled: true
+		, color: '#EC1813'
+	}
+};
